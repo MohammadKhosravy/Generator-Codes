b70721abd1e98d3404fcdcf2d98f3c5970153943
MohammadKhosravy <mohammad.khosravi@uni-segen.de>
2022-05-11 16:15:29 +0200

    RR-CB is updated

---
+++ RR-CB/sel.h
@@ -0,0 +1,28 @@
+#ifndef _SEL_H_
+#define _SEL_H_
+
+#include <vector>
+
+class Sel
+{
+    public:
+        void generate_RR_CB_U(int _n, int _p, int _gamma);
+        void generate_RR_CB_1(int _n, int _p, int _gamma);
+        void generate_RR_CB_2(int _n, int _p, int _gamma);
+
+        std::vector<int> get_C();
+        std::vector<int> get_c();
+        std::vector<int> get_d();
+
+    private:
+
+        int n, p;
+
+        int gamma;
+
+        std::vector<int> C;
+        std::vector<int> c;
+        std::vector<int> d;
+};
+
+#endif
