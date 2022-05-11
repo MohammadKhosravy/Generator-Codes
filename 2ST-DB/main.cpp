94f80d7958914b38c11fe318111c1113622b2589
MohammadKhosravy <mohammad.khosravi@uni-segen.de>
2022-05-11 16:14:03 +0200

    2ST-DB is updated

---
+++ 2ST-DB/main.cpp
@@ -0,0 +1,54 @@
+#include "sel.h"
+#include <iostream>
+#include <cstdlib>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    Sel sel;
+
+    srand(atoi(argv[5]));
+
+    int n = atoi(argv[1]);
+    int p = atoi(argv[2]);
+    int gamma = atoi(argv[3]);
+    int param = atoi(argv[4]);
+
+
+    if (param == 0)
+		sel.generate_2ST_DB_U(n,p,gamma);
+    else if (param == 1)
+		sel.generate_2ST_DB_1(n,p,gamma);
+  	else if (param == 2)
+    sel.generate_2ST_DB_2(n,p,gamma);
+    else
+    cout << "Warning: the fourth argument should be 0, 1 or 2" << endl;
+
+    vector<int> C = sel.get_C();
+    vector<int> c = sel.get_c();
+    vector<int> d = sel.get_d();
+
+    stringstream ss;
+	  ss<<"instance";
+	  for (int i=0; i<5; ++i)
+		ss<<"-"<<argv[i+1];
+	  ss<<".dat";
+
+  	ofstream out (ss.str().c_str());
+	  out<<n<<";"<<p<<";"<<gamma<<"\n";
+	  for (int i=0; i<n; ++i)
+        out<<C[i]<<";";
+    out<<"\n";
+	  for (int i=0; i<n; ++i)
+        out<<c[i]<<";";
+    out<<"\n";
+    for (int i=0; i<n; ++i)
+        out<<d[i]<<";";
+    out<<"\n";
+	  out.close();
+
+    return 0;
+}
---
+++ 2ST-DB/sel.cpp
@@ -0,0 +1,92 @@
+
+#include "sel.h"
+#include <set>
+#include <iostream>
+#include "ilcplex/ilocplex.h"
+
+ILOSTLBEGIN
+
+using namespace std;
+
+void Sel::generate_2ST_DB_U(int _n, int _p, int _gamma)
+{
+    n = _n;
+    p = _p;
+    gamma = _gamma;
+
+    C.resize(n);
+    c.resize(n);
+    d.resize(n);
+
+	for(int i=0; i<n; ++i)
+    {
+        C[i] = rand()%100+1;
+        c[i] = rand()%100+1;
+        d[i] = rand()%100+1;
+    }
+
+}
+
+void Sel::generate_2ST_DB_1(int _n, int _p, int _gamma)
+{
+    n = _n;
+    p = _p;
+    gamma = _gamma;
+
+    C.resize(n);
+    c.resize(n);
+    d.resize(n);
+
+    for(int i=0; i<n; ++i)
+    {
+        C[i] = rand()%100+1;
+        c[i] = rand()%10+1;
+    }
+
+	for(int i=0; i<n; ++i)
+        d[i] = rand()%(c[i]+1)+(100-c[i]);
+
+}
+
+void Sel::generate_2ST_DB_2(int _n, int _p, int _gamma)
+{
+    n = _n;
+    p = _p;
+    gamma = _gamma;
+
+    C.resize(n);
+    c.resize(n);
+    d.resize(n);
+
+    for(int i=0; i<n; ++i)
+        C[i] = rand()%100+1;
+
+    for(int i=0; i<n; ++i)
+        c[i] = 100 - C[i];
+
+	for(int i=0; i<n; ++i)
+        d[i] = rand()%(101-c[i])+(c[i]);
+
+}
+
+
+
+vector<int> Sel::get_C()
+{
+	return C;
+}
+
+
+
+vector<int> Sel::get_c()
+{
+	return c;
+}
+
+
+
+vector<int> Sel::get_d()
+{
+	return d;
+}
+
